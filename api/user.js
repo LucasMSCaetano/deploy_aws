f0e3b833770004b97b8ae912e68e566ff56e6cdb
unknown <lucas.matheu.caetano@gmail.com>
2020-11-02 18:20:20 -0300

    primeiro envio

---
+++ api/user.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt-nodejs');
+const passport = require('passport');
+
+module.exports = (app) => {
+  const obeterHash = (password, callback) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, null, (err, hash) => callback(hash));
+    });
+  };
+  const save = (req, res) => {
+    obeterHash(req.body.password, (hash) => {
+      const password = hash;
+
+      app
+        .db('users')
+        .insert({
+          name: req.body.name,
+          email: req.body.email.toLowerCase(),
+          password,
+        })
+        .then((_) => res.status(204).send())
+        .catch((err) => res.status(400).json(err));
+    });
+  };
+
+  return { save };
+};
